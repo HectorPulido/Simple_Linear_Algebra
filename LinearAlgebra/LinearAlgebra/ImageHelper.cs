3d00b94c455a5f21e7879be7d8b2cd15833714f9
Hector Pulido <Hectorandrespp@gmail.com>
2018-01-17 01:49:57 -0500

    Square brackets available
    Performance Improvement
    Square brackets
    Max Function
    Flat Function
    ToString function simplified

--- LinearAlgebra/LinearAlgebra/ImageHelper.cs
+++ LinearAlgebra/LinearAlgebra/ImageHelper.cs
@@ -1,22 +1,31 @@
 ﻿using System;
 using System.Drawing;
-
+using System.Drawing.Imaging;
 namespace LinearAlgebra
 {
-    class ImageHelper
+    public static class ImageHelper
     {
-        public static Matrix[] LoadImage(string directory, double SizeX, double SizeY)
+
+        static Color c;
+        static Bitmap img;
+
+        public static Bitmap ResizeImage(Bitmap _img, double SizeX, double SizeY)
         {
-            Bitmap _img = (Bitmap)Image.FromFile(directory);
             var brush = new SolidBrush(Color.Black);
             float scale = Math.Min((float)SizeX / _img.Width, (float)SizeY / _img.Height);
-            var img = new Bitmap((int)SizeX, (int)SizeY);
+
+            img = new Bitmap((int)SizeX, (int)SizeY);
+
             var graph = Graphics.FromImage(img);
             int scaleWidth = (int)(_img.Width * scale);
             int scaleHeight = (int)(_img.Height * scale);
             graph.FillRectangle(brush, new RectangleF(0, 0, (float)SizeX, (float)SizeY));
             graph.DrawImage(_img, new Rectangle((int)((SizeX - scaleWidth) / 2.0), (int)((SizeY - scaleHeight) / 2.0), (int)SizeX, (int)SizeY));
 
+            return img;
+        }
+        public static Matrix[] ImageToMatrix(Bitmap img)
+        {
             int h = img.Height;
             int w = img.Width;
             double[,] r = new double[h, w];
@@ -25,62 +34,67 @@ namespace LinearAlgebra
             double[,] a = new double[h, w];
             double[,] bw = new double[h, w];
             Matrix.MatrixLoop((i, j) => {
-                var color = img.GetPixel(i, j);
-                r[i, j] = color.R;
-                g[i, j] = color.G;
-                b[i, j] = color.B;
-                a[i, j] = color.A;
-                bw[i, j] = color.GetBrightness() * 255;
+                c = img.GetPixel(i, j);
+                r[i, j] = c.R;
+                g[i, j] = c.G;
+                b[i, j] = c.B;
+                a[i, j] = c.A;
+                bw[i, j] = c.GetBrightness() * 255;
             }, h, w);
             return new Matrix[] { bw, r, g, b };
+        }
+        public static Matrix[] LoadImage(string directory, double SizeX, double SizeY)
+        {
+            img = ResizeImage((Bitmap)Image.FromFile(directory), SizeX, SizeY);
+            return ImageToMatrix(img);
         }
         public static Matrix[] LoadImage(string directory)
         {
-            Bitmap img = (Bitmap)Image.FromFile(directory);
-            int h = img.Height;
-            int w = img.Width;
-            double[,] r = new double[h, w];
-            double[,] g = new double[h, w];
-            double[,] b = new double[h, w];
-            double[,] a = new double[h, w];
-            double[,] bw = new double[h, w];
-            Matrix.MatrixLoop((i, j) => {
-                var color = img.GetPixel(i, j);
-                r[i, j] = color.R;
-                g[i, j] = color.G;
-                b[i, j] = color.B;
-                a[i, j] = color.A;
-                bw[i, j] = color.GetBrightness() * 255;
-            }, h, w);
-            return new Matrix[] { bw, r, g, b };
+            img = (Bitmap)Image.FromFile(directory);
+            return ImageToMatrix(img);
         }
-        public static void SaveImage(Matrix bw, string directory)
+        public static void SaveImage(double[,] bw, string directory)
         {
-            Bitmap img = new Bitmap(bw.x, bw.y);
+            img = new Bitmap(bw.GetLength(0), bw.GetLength(1));
             Matrix.MatrixLoop((i, j) => {
-                Color c = Color.FromArgb((int)bw.matrix[i, j], (int)bw.matrix[i, j], (int)bw.matrix[i, j]);
+                c = Color.FromArgb(
+                    (int)clamp(bw[i, j], 255, 0), 
+                    (int)clamp(bw[i, j], 255, 0), 
+                    (int)clamp(bw[i, j], 255, 0));
                 img.SetPixel(i, j, c);
-            }, bw.x, bw.y);
-            img.Save(directory, System.Drawing.Imaging.ImageFormat.Bmp);
+            }, bw.GetLength(0), bw.GetLength(1));
+            img.Save(directory, ImageFormat.Bmp);
         }
-        public static void SaveImage(Matrix r, Matrix g, Matrix b, string directory)
+        public static void SaveImage(double[,] r, double[,] g, double[,] b, string directory)
         {
-            Bitmap img = new Bitmap(r.x, r.y);
+            img = new Bitmap(r.GetLength(0), r.GetLength(1));
             Matrix.MatrixLoop((i, j) => {
-                Color c = Color.FromArgb((int)r.matrix[i, j], (int)g.matrix[i, j], (int)b.matrix[i, j]);
+                c = Color.FromArgb(
+                    (int)clamp(r[i, j], 255, 0), 
+                    (int)clamp(g[i, j], 255, 0), 
+                    (int)clamp(b[i, j], 255, 0));
                 img.SetPixel(i, j, c);
-            }, r.x, r.y);
-            img.Save(directory, System.Drawing.Imaging.ImageFormat.Bmp);
+            }, r.GetLength(0), r.GetLength(1));
+            img.Save(directory, ImageFormat.Bmp);
         }
-        public static void SaveImage(Matrix r, Matrix g, Matrix b, Matrix a, string directory)
+        public static void SaveImage(double[,] a, double[,] r, 
+            double[,] g, double[,] b, string directory)
         {
-            Bitmap img = new Bitmap(r.x, r.y);
+            img = new Bitmap(r.GetLength(0), r.GetLength(1));
             Matrix.MatrixLoop((i, j) => {
-                Color c = Color.FromArgb((int)a.matrix[i, j], (int)r.matrix[i, j],
-                    (int)g.matrix[i, j], (int)b.matrix[i, j]);
+                c = Color.FromArgb(
+                    (int)clamp(a[i, j], 255, 0),
+                    (int)clamp(r[i, j], 255, 0),
+                    (int)clamp(g[i, j], 255, 0),
+                    (int)clamp(b[i, j], 255, 0));
                 img.SetPixel(i, j, c);
-            }, r.x, r.y);
-            img.Save(directory, System.Drawing.Imaging.ImageFormat.Bmp);
+            }, r.GetLength(0), r.GetLength(1));
+            img.Save(directory, ImageFormat.Bmp);
         }
+        static double clamp(double d, double max, double min)
+        {
+            return d > max ? max : d < min ? min : d;
+        }
+
     }
 }
